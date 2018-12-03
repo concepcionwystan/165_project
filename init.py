262f4d1d60fe7e398bbda066f133b3b9ab3d7a18
Wystan <concepcionwystan@gmail.com>
2018-12-03 11:20:27 +0800

    lahat

--- init.py
+++ init.py
@@ -1,58 +1,107 @@
-from main import db,Artist,Position,Group,Album,Song,Member
+from main import db,Artist,Group
 db.create_all()
 
-artist = Artist(birth_name="Kim Ji Soo",stage_name="Jisoo",birthday="789096283")
-db.session.add(artist)
-artist = Artist(birth_name="Jennie Kim",stage_name="Jennie",birthday="821755483")
-db.session.add(artist)
-artist = Artist(birth_name="Park Chae Young",stage_name="Rose",birthday="855624283")
-db.session.add(artist)
-artist = Artist(birth_name="Lalisa Manoban/ Pranpriya Manoban",stage_name="Lisa",birthday="859425883")
-db.session.add(artist)
+# artist = Artist(birth_name="Kim Ji Soo",stage_name="Jisoo",birthday="789096283")
+# db.session.add(artist)
+# artist = Artist(birth_name="Jennie Kim",stage_name="Jennie",birthday="821755483")
+# db.session.add(artist)
+# artist = Artist(birth_name="Park Chae Young",stage_name="Rose",birthday="855624283")
+# db.session.add(artist)
+# artist = Artist(birth_name="Lalisa Manoban/ Pranpriya Manoban",stage_name="Lisa",birthday="859425883")
+# db.session.add(artist)
 
-db.session.commit()
+# db.session.commit()
 
 
 
-position = Position(position_name="Visual",artist_id=1)
-db.session.add(position)
-position = Position(position_name="Main Rapper",artist_id=2)
-db.session.add(position)
-position = Position(position_name="Main Vocal",artist_id=3)
-db.session.add(position)
-position = Position(position_name="Main Dancer",artist_id=4)
-db.session.add(position)
+# position = Position(position_name="Visual",artist_id=1)
+# db.session.add(position)
+# position = Position(position_name="Main Rapper",artist_id=2)
+# db.session.add(position)
+# position = Position(position_name="Main Vocal",artist_id=3)
+# db.session.add(position)
+# position = Position(position_name="Main Dancer",artist_id=4)
+# db.session.add(position)
 
-db.session.commit()
+# db.session.commit()
 
-group = Group(group_name="BLACKPINK",debut_date="1470619483",fandom_name="BLINK")
+group = Group(groupName="NCT 127",groupType="Boy Group",debutDate="July 27, 2016",company="SM Entertainment",fandomName="NCTzen",
+	fandomColor="-",status="active",imgURL="https://is4-ssl.mzstatic.com/image/thumb/Music118/v4/63/cc/b4/63ccb404-eb7a-a3f0-4dc9-152540bc161f/NCT_127_Cover.jpg/268x0w.jpg",
+	description="NCT 127 (엔씨티 127) is the 2nd sub-unit of the boy group NCT. The sub-unit currently consists of 10 'members': Taeil, Johnny, Taeyong, Doyoung, Yuta, Jaehyun, Win Win, Jungwoo, Mark, Haechan. NCT 127 debuted on July 7th, 2016 under SM Entertainment.")
 db.session.add(group)
+group = Group(groupName="Stray Kids",groupType="Boy Group",debutDate="September 13, 2018",company="JYP Entertainment",fandomName="Stay",
+	fandomColor="-",status="active",imgURL="https://lastfm-img2.akamaized.net/i/u/770x0/dd9393d96f6b5b33cdd6e90f35b680fd.jpg",
+	description="Stray Kids (스트레이 키즈) is a 9-member South Korean boy group under JYP Entertainment. The group consists of Bang Chan, Woojin, Lee Know, Changbin, Hyunjin, Han, Felix, Seungmin, and I.N. Stray Kids was created through the survival program with the same name, Stray Kids. Stray Kids debuted on March 25, 2018.")
+db.session.add(group)
+group = Group(groupName="LOONA",groupType="Girl Group",debutDate="August 20, 2018",company="Blockberry Creative",fandomName="Orbit",
+	fandomColor="Holo",status="active",imgURL="https://lastfm-img2.akamaized.net/i/u/ar0/7e9fbb03749066e1d58b6e9261fdbbbc.jpg",
+	description="LOONA (LOOΠΔ – 이달의 소녀) contains of 12 'members': Haseul, Vivi, Yves, JinSoul, Kim Lip, Chuu, Heejin, Hyunjin, Go Won, Choerry, Olivia Hye and Yeojin. The band is under Blockberry Creative. They debuted on August 20, 2018 with their title track “Hi High”.")
+db.session.add(group)
+group = Group(groupName="Seventeen",groupType="Boy Group",debutDate="",company="Pledis",fandomName="Carat",
+	fandomColor="Rose Quartz and Serenity",status="active",imgURL="https://i.scdn.co/image/7b90f0bfdce832e113edba285d7aad9c283eb3d7",
+	description="Saks lang")
+db.session.add(group)
+group = Group(groupName="SNSD",groupType="Girl Group",debutDate="",company="",fandomName="SONE",
+	fandomColor="",status="",imgURL="https://lastfm-img2.akamaized.net/i/u/ar0/6cb5367bf92f061d3bfac9277161e3c2.jpg",
+	description="I love you Taeyeon")
+db.session.add(group)
+group = Group(groupName="The Boyz",groupType="Boy Group",debutDate="",company="",fandomName="",
+	fandomColor="",status="",imgURL="https://pbs.twimg.com/profile_images/942318800480772096/z8zfdksu_400x400.jpg",
+	description="Pweds na")
+db.session.add(group)
+db.session.commit()
+
+artist = Artist(stageName="Hyunjin",birthName="Hwang Hyunjin",nationality="Korean",gender="Male",birthday="",groupId=2,
+	imgURL="https://66.media.tumblr.com/37b02b0fea4e8a3168e8e00d2c9f7631/tumblr_inline_ozl6248sJX1uprypi_540.gif")
+db.session.add(artist)
+artist = Artist(stageName="Woojin",birthName="Im Jaebum",nationality="Korean",gender="Male",birthday="",groupId=2,
+	imgURL="https://media1.tenor.com/images/ace5f2fdd8d31c89e0c277185c5ef4a7/tenor.gif?itemid=12066202")
+db.session.add(artist)
+artist = Artist(stageName="Chan",birthName="Im Jaebum",nationality="Korean",gender="Male",birthday="",groupId=2,
+	imgURL="https://pa1.narvii.com/6644/cbd22089050ae0a3c7c25daac0a8ccf750b37ba2_hq.gif")
+db.session.add(artist)
+artist = Artist(stageName="Felix",birthName="Im Jaebum",nationality="Korean",gender="Male",birthday="",groupId=2,
+	imgURL="https://66.media.tumblr.com/e1c932ec2735439ace7261395f7c61c7/tumblr_pfcxbmi6yL1xo1fixo1_500.gif")
+db.session.add(artist)
+artist = Artist(stageName="Han",birthName="Im Jaebum",nationality="Korean",gender="Male",birthday="",groupId=2,
+	imgURL="https://pa1.narvii.com/6644/cbd22089050ae0a3c7c25daac0a8ccf750b37ba2_hq.gif")
+db.session.add(artist)
+artist = Artist(stageName="I.N.",birthName="Im Jaebum",nationality="Korean",gender="Male",birthday="",groupId=2,
+	imgURL="https://pa1.narvii.com/6694/de1e3094a68caf86d197e0663ae29db9be108102_hq.gif")
+db.session.add(artist)
+artist = Artist(stageName="Mingyu",birthName="Kim Mingyu",nationality="Korean",gender="Male",birthday="",groupId=4,
+	imgURL="https://i.pinimg.com/originals/57/18/65/57186539846981fcc7645600595e99bc.gif")
+db.session.add(artist)
+artist = Artist(stageName="Kim Lip",birthName="Kim Jungeun",nationality="Korean",gender="Female",birthday="",groupId=3,
+	imgURL="https://66.media.tumblr.com/5efc552c2a9106b9374c65cf81007868/tumblr_oyvfudQ2cl1qh3sm2o1_500.gif")
+db.session.add(artist)
+
 
 db.session.commit()
 
-member = Member(artist_id=1,group_name="BLACKPINK")
-db.session.add(member)
-member = Member(artist_id=2,group_name="BLACKPINK")
-db.session.add(member)
-member = Member(artist_id=3,group_name="BLACKPINK")
-db.session.add(member)
-member = Member(artist_id=4,group_name="BLACKPINK")
-db.session.add(member)
+# member = Member(artist_id=1,group_name="BLACKPINK")
+# db.session.add(member)
+# member = Member(artist_id=2,group_name="BLACKPINK")
+# db.session.add(member)
+# member = Member(artist_id=3,group_name="BLACKPINK")
+# db.session.add(member)
+# member = Member(artist_id=4,group_name="BLACKPINK")
+# db.session.add(member)
 
-db.session.commit()
+# db.session.commit()
 
-album = Album(album_title="Square Up",release_date="1529020800",sales=90000000,spotify_url="spotify:album:1HwIUaaEuRsxsIyssqtGLH",group_name="BLACKPINK")
-db.session.add(album)
+# album = Album(album_title="Square Up",release_date="1529020800",sales=90000000,spotify_url="spotify:album:1HwIUaaEuRsxsIyssqtGLH",group_name="BLACKPINK")
+# db.session.add(album)
 
-db.session.commit()
+# db.session.commit()
 
-song = Song(song_title="Ddu-Du Ddu-Du",album_id=1)
-db.session.add(song)
-song = Song(song_title="Forever Young",album_id=1)
-db.session.add(song)
-song = Song(song_title="Really",album_id=1)
-db.session.add(song)
-song = Song(song_title="See U Later",album_id=1)
-db.session.add(song)
+# song = Song(song_title="Ddu-Du Ddu-Du",album_id=1)
+# db.session.add(song)
+# song = Song(song_title="Forever Young",album_id=1)
+# db.session.add(song)
+# song = Song(song_title="Really",album_id=1)
+# db.session.add(song)
+# song = Song(song_title="See U Later",album_id=1)
+# db.session.add(song)
 
-db.session.commit()
+# db.session.commit()
--- main.py
+++ main.py
@@ -7,43 +7,47 @@ from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@localhost/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://postgres:password@localhost/postgres'
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.secret_key = 'my_secret_key'
 db = SQLAlchemy(app)
 
+class Group(db.Model):
+    id = db.Column(db.Integer, unique=True,autoincrement=True,primary_key=True)
+    groupName = db.Column(db.String(50), unique=True)
+    groupType = db.Column(db.String(80))
+    company = db.Column(db.String(80))
+    debutDate = db.Column(db.String(80))
+    fandomName = db.Column(db.String(80))
+    fandomColor = db.Column(db.String(80))
+    status = db.Column(db.String(80))
+    imgURL = db.Column(db.String(500))
+    description = db.Column(db.String(500))
+
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
 class Artist(db.Model):
-    artist_id = db.Column(db.Integer, unique=True,primary_key = True,autoincrement=True)
-    birth_name = db.Column(db.String(80))
-    stage_name = db.Column(db.String(80), nullable=False)
+    id = db.Column(db.Integer, unique=True,primary_key = True,autoincrement=True)
+    birthName = db.Column(db.String(80))
+    stageName = db.Column(db.String(80), nullable=False)
     birthday = db.Column(db.String(80))
-    solo_debut_date = db.Column(db.String(80))
+    nationality = db.Column(db.String(80))
+    gender = db.Column(db.String(80))
+    groupId = db.Column(db.Integer)
+    imgURL = db.Column(db.String(500))
 
     def as_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 class Member(db.Model):
-    artist_id = db.Column(db.Integer,db.ForeignKey('artist.artist_id'),primary_key=True)
+    artist_id = db.Column(db.Integer,db.ForeignKey('artist.id'),primary_key=True)
     group_name = db.Column(db.String(50),primary_key = True)
 
     def as_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
-class Position(db.Model):
-	position_id = db.Column(db.Integer, unique=True,primary_key = True,autoincrement=True)
-	position_name = db.Column(db.String(50))
-	artist_id = db.Column(db.Integer,db.ForeignKey('artist.artist_id'))
-	def as_dict(self):
-		return {c.name: getattr(self, c.name) for c in self.__table__.columns}
-
-class Group(db.Model):
-    group_name = db.Column(db.String(50), unique=True,primary_key = True)
-    debut_date = db.Column(db.String(80))
-    fandom_name = db.Column(db.String(80))
-    official_color = db.Column(db.String(80))
-
-    def as_dict(self):
-       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 class Album(db.Model):
     album_id = db.Column(db.Integer, unique=True,primary_key = True,autoincrement=True)
@@ -51,7 +55,7 @@ class Album(db.Model):
     release_date = db.Column(db.String(80))
     sales = db.Column(db.Integer)
     spotify_url = db.Column(db.String)
-    group_name = db.Column(db.String(50),db.ForeignKey('group.group_name'))
+    group_name = db.Column(db.String(50),db.ForeignKey('group.groupName'))
         
     def as_dict(self):
        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
@@ -74,10 +78,141 @@ class Song(db.Model):
 def hello():
     return "Homepage"
 
-@app.route("/add/group",methods=["GET"])
+
+@app.route("/artists",methods=["GET"])
+@cross_origin()
+def list_artists():
+    groupID = request.args.get('groupId')
+    if groupID == None:
+        artists = Artist.query.all()
+        list_artists = []
+        for artist in artists:
+            list_artists.append(artist.as_dict())
+        return jsonify(list_artists)
+    else:
+        artists = Artist.query.filter_by(groupID=groupID).all()
+        list_artists = []
+        for artist in artists:
+            list_artists.append(artist.as_dict())
+        return jsonify(list_artists)
+
+
+
+
+
+@app.route("/groups",methods=["GET"])
+@cross_origin()
+def list_groups():
+    groups = Group.query.all()
+    list_groups = []
+    for group in groups:
+        list_groups.append(group.as_dict())
+    return jsonify(list_groups)
+
+@app.route("/groups/<int:groupID>",methods=["DELETE"])
+@cross_origin()
+def delete_group(groupID):
+    group = Group.query.filter_by(id=groupID).first()
+    db.session.delete(group)
+    db.session.commit()
+    return jsonify(success=True,status_code = 200)
+
+@app.route("/artists/<int:artistID>",methods=["DELETE"])
+@cross_origin()
+def delete_artist(artistID):
+    artist = Artist.query.filter_by(id=artistID).first()
+    db.session.delete(artist)
+    db.session.commit()
+    return jsonify(success=True,status_code = 200)
+
+@app.route("/groups/",methods=["POST"])
 @cross_origin()
 def add_group():
-	return render_template("add_group.html")
+    data = request.get_json()
+    groupName = data['groupName']
+    groupType = data['groupType']
+    company = data['company']
+    debutDate = data['debutDate']
+    status = data['status']
+    fandomName = data['fandomName']
+    fandomColor = data['fandomColor']
+    imgURL = data['imgURL']
+    description = data['description']
+    group = Group(groupName=groupName,groupType=groupType,company=company,debutDate=debutDate,status=status,fandomName=fandomName,
+        fandomColor=fandomColor,imgURL=imgURL,description=description)
+    db.session.add(group)
+    db.session.commit()
+    return jsonify(success=True,status_code = 200)
+
+
+@app.route("/artists/",methods=["POST"])
+@cross_origin()
+def add_artist():
+    data = request.get_json()
+    stageName = data['stageName']
+    birthName = data['birthName']
+    birthday = data['birthday']
+    gender = data['gender']
+    nationality = data['nationality']
+    imgURL = data['imgURL']
+    groupId = data['groupId']
+    artist = Artist(stageName=stageName,birthName=birthName,birthday=birthday,gender=gender,nationality=nationality,imgURL=imgURL,
+        groupId=groupId)
+    db.session.add(artist)
+    db.session.commit()
+    return jsonify(success=True,status_code = 200)
+
+@app.route("/artists/<int:artistID>",methods=["GET","PUT"])
+@cross_origin()
+def list_artist(artistID):
+    if request.method == 'PUT':
+        data = request.get_json()
+        stageName = data['stageName']
+        birthName = data['birthName']
+        birthday = data['birthday']
+        gender = data['gender']
+        nationality = data['nationality']
+        imgURL = data['imgURL']
+        groupId = data['groupId']
+        artist = Artist.query.filter_by(id=artistID).first()
+        artist.stageName = stageName
+        artist.birthName = birthName
+        artist.birthday = birthday
+        artist.gender = gender
+        artist.nationality = nationality
+        artist.imgURL = imgURL
+        artist.groupId = groupId
+        db.session.commit()
+    artist = Artist.query.filter_by(id=artistID).first()
+    return jsonify(artist.as_dict())
+
+@app.route("/groups/<int:groupID>",methods=["GET","PUT"])
+@cross_origin()
+def list_group(groupID):
+    if request.method == 'PUT':
+        data = request.get_json()
+        groupName = data['groupName']
+        groupType = data['groupType']
+        company = data['company']
+        debutDate = data['debutDate']
+        status = data['status']
+        fandomName = data['fandomName']
+        fandomColor = data['fandomColor']
+        imgURL = data['imgURL']
+        description = data['description']
+        group = Group.query.filter_by(id=groupID).first()
+        group.groupName = groupName
+        group.groupType = groupType
+        group.company = company
+        group.debutDate = debutDate
+        group.status = status
+        group.fandomName = fandomName
+        group.fandomColor = fandomColor
+        group.imgURL = imgURL
+        group.description = description
+        db.session.commit()
+    group = Group.query.filter_by(id=groupID).first()
+    return jsonify(group.as_dict())
 
 
 if __name__ == "__main__":
